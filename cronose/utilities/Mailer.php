42db918a6e2d922380d4b47f794f852739cf533a
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-02-14 12:26:09 +0100

    Mailer v2

--- cronose/utilities/Mailer.php
+++ cronose/utilities/Mailer.php
@@ -5,7 +5,7 @@ class Mailer {
   private static $defaultFrom = "cronose@cronose.dawman.info";
 
   public static function sendMailTo($subject, $message, $to, $from = null, $headers = "") {
-    $from = $from || self::$defaultFrom;
+    $from = $from ?? self::$defaultFrom;
     $headers.= "From: ${from}\r\n";
     $message = wordwrap($message, 70);
     mail($to, $subject, $message, $headers);
