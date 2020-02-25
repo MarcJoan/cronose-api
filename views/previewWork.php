ae199cce779f5358aaf5ee33a35fd2672518966f
Marc Joan Cabrer Rosselló <marcjoancr@gmail.com>
2020-02-25 10:42:34 +0100

    Deleting MVC views

--- views/previewWork.php
+++
@@ -1,8 +0,0 @@
-<?php require 'layouts/head.php';?>
-
-<h1>Preview Work</h1>
-
-<a href="/published">Publish</a>
-
-
-<?php require '../views/layouts/footer.php';?>
--- views/published.php
+++
@@ -1,6 +0,0 @@
-<?php require 'layouts/head.php';?>
-
-<h1>Published!</h1>
-
-
-<?php require '../views/layouts/footer.php';?>
