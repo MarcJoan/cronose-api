ac8466d1c451906b5188d96b093cd0ce58df6356
josep <josep.oliverr@gmail.com>
2020-03-03 18:01:11 +0100

    Updating image controller

--- controllers/Image.controller.php
+++ controllers/Image.controller.php
@@ -6,7 +6,7 @@ class ImageController {
 
   public static function saveImages($user_initials, $user_tag, $files){
     foreach ($files as $key => &$file) {
-      $dir = "images/" . strval($key) ."/";
+      $dir = "images/" . strval($key);
       $name = "${key}_${user_initials}_${user_tag}";
       $fullUrl = $key.'/'.$name;
       move_uploaded_file ( $file['tmp_name'] , $dir.'/'.$name );
