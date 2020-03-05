bb344b09c899c61feb84d6d7c11f8a7dc82bb2b3
Miguel Angel <mena_alcalde@hotmail.com>
2020-03-05 22:54:46 +0100

    work

--- public/index.php
+++ public/index.php
@@ -116,7 +116,7 @@ $router->post('/login', function() {
 $router->get('/works', function() {
   echo json_encode(WorkController::getAllWorks());
 });
-$router->get('/works/user/{id}', function($user_id) {
+$router->get('/works/user/{user_id}', function($user_id) {
   echo json_encode(WorkController::getAllWorksByUser($user_id));
 });
 $router->post('/works/filter', function() {
